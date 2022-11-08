3f71ac26171d08384bfdb20690e1675d6d4e907c
MADHAV MAHESHWARI <87849042+madhav-293@users.noreply.github.com>
2022-11-08 13:28:27 +0530

    Update solution.java

--- 1. Two Sum/solution.java
+++ 1. Two Sum/solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> hash=new HashMap<>();
+        HashMap<Integer,Integer> hash=new HashMap<>(); //implementation of HashMap
         for(int i=0;i<nums.length;i++){
             int c=target-nums[i];
             if(hash.containsKey(c))
