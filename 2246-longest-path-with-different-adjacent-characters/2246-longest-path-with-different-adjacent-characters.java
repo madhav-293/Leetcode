be8edd739c85d6997d7b138f7bdcd5b5d2a5fcbe
Madhav Maheshwari <87849042+madhav-293@users.noreply.github.com>
2023-01-13 19:27:09 +0530

    Time: 166 ms (65.13%), Space: 91.9 MB (92.44%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.java
@@ -1,48 +1,38 @@
 class Solution {
-    private int longestPath = 1;
-
-    public int dfs(int currentNode, Map<Integer, List<Integer>> children, String s) {
-        // If the node is the only child, return 1 for the currentNode itself.
-        if (!children.containsKey(currentNode)) {
-            return 1;
-        }
-
-        // Longest and second longest path starting from currentNode (does not count the
-        // currentNode itself).
-        int longestChain = 0, secondLongestChain = 0;
-        for (int child : children.get(currentNode)) {
-            // Get the number of nodes in the longest chain starting from the child,
-            // including the child.
-            int longestChainStartingFromChild = dfs(child, children, s);
-            // We won't move to the child if it has the same character as the currentNode.
-            if (s.charAt(currentNode) == s.charAt(child)) {
-                continue;
-            }
-            // Modify the longestChain and secondLongestChain if longestChainStartingFromChild
-            // is bigger.
-            if (longestChainStartingFromChild > longestChain) {
-                secondLongestChain = longestChain;
-                longestChain = longestChainStartingFromChild;
-            } else if (longestChainStartingFromChild > secondLongestChain) {
-                secondLongestChain = longestChainStartingFromChild;
-            }
-        }
-
-        // Add "1" for the node itself.
-        longestPath = Math.max(longestPath, longestChain + secondLongestChain + 1);
-        return longestChain + 1;
-    }
+    int longestPathValue = 1; // variable to store the length of the longest path
 
     public int longestPath(int[] parent, String s) {
-        int n = parent.length;
-        Map<Integer, List<Integer>> children = new HashMap<>();
-        // Start from node 1, since root node 0 does not have a parent.
-        for (int i = 1; i < n; i++) {
-            children.computeIfAbsent(parent[i], value -> new ArrayList<Integer>()).add(i);
+        // create an adjacency list representation of the tree
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        for(int i = 1; i < parent.length; i++){
+            int j = parent[i];
+            adj.putIfAbsent(j, new LinkedList<>());
+            adj.get(j).add(i);
         }
+        // call dfs on the root of the tree
+        dfs(0, adj, s);
+        return longestPathValue;
+    }
 
-        dfs(0, children, s);
-
-        return longestPath;
+    public int dfs(int node, Map<Integer, List<Integer>> adj, String s){
+        // if the node is a leaf node, return 1
+        if(!adj.containsKey(node)) return 1;
+        int max = 0, secondMax = 0;
+        // for each neighbor of the node
+        for(int nbrNode : adj.get(node)){
+            int longestPathFromNbrNode = dfs(nbrNode , adj, s);
+            // if the characters at the current node and its neighbor are the same, ignore the neighbor
+            if(s.charAt(node) == s.charAt(nbrNode)) continue;
+            // update max and secondMax with the longest path from the neighbor node
+            if(longestPathFromNbrNode > max){
+                secondMax = max;
+                max = longestPathFromNbrNode;
+            }else if(longestPathFromNbrNode > secondMax){
+                secondMax = longestPathFromNbrNode;
+            }
+        }
+        // update longestPathValue with the longest path that includes the current node
+        longestPathValue = Math.max(longestPathValue, max+secondMax+1);
+        return max+1;
     }
 }
