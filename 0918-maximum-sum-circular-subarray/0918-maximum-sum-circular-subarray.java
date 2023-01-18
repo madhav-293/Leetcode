59dc8053ac6a9eca625d18961556566701737897
Madhav Maheshwari <87849042+madhav-293@users.noreply.github.com>
2023-01-18 23:28:26 +0530

    Time: 3 ms (91.89%), Space: 47.7 MB (68.25%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        //calculating max Subarray sum using kadane's Algo (linear array)
+        int max=Integer.MIN_VALUE, sum=0, total=0;
+        for(int x:nums){
+            sum+=x;
+            max=Math.max(max,sum);
+            if(sum<0)
+                sum=0;
+            total+=x; //calculating sum of elements of nums
+        }
+        
+        int max2=0;
+        sum=0;
+        for(int x:nums){
+            max=Math.max(max,total+max2); //updating max when right sum + x + max left sum>max
+            total-=x; //right sum
+            sum+=x; //left sum  
+            max2=Math.max(max2,sum); //maximum left sum
+        }
+        return max;
+    }
+}
