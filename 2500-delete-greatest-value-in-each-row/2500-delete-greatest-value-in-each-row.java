07808ee17d6db52c91ae20fae100f1c42e0daeeb
Madhav Maheshwari <87849042+madhav-293@users.noreply.github.com>
2023-01-24 15:41:01 +0530

    Time: 3 ms (99.17%), Space: 42.3 MB (85.76%) - LeetHub

--- 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.java
@@ -1,24 +1,18 @@
 class Solution {
     public int deleteGreatestValue(int[][] grid) {
-        //firstly observe a pattern that each time we take every row maximum which can be achieved by sorting and each time we did comparison from each row
-       for(int i[]:grid){
-           //sorting
-           Arrays.sort(i);
-       }
-       //now we did comparsion column no. of times
-       int ans = 0;
-       for(int j=0;j<grid[0].length;j++){
-           int m = -1;
-           //each row one by one we traverse
-           for(int row[]:grid){
-               //we match the pattern while doing comparsion 
-               //in every jth comparsion we take jth element of the row
-               m = Math.max(m,row[j]);
-           }
-           ans += m;
-       }
+        int sum=0;
+        for(int i =0;i<grid.length;i++){
+            Arrays.sort(grid[i]);
+        }
 
-       return ans;
+        for(int i= grid[0].length-1;i>=0;i--){
+            int max = 0;
+            for(int j=0;j<grid.length;j++){
 
-       }
+                max = Math.max(max, grid[j][i]);
+            }
+            sum+=max;
+        }
+        return sum;
     }
+}
