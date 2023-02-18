c0e460d8811ccd8081d2443d2334ac23d6ba92ae
Madhav Maheshwari <87849042+madhav-293@users.noreply.github.com>
2023-02-18 11:36:46 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (70.97%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,8 +1,5 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double arr[]=new double[2];
-        arr[0]=celsius+273.15;
-        arr[1]=celsius*1.8+32.0;
-        return arr;
+        return new double[]{celsius + 273.15, (celsius * 1.80) + 32.00};
     }
 }
