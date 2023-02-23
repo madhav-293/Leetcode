91a072b8cdd75f967af18912b066637f877e511d
Madhav Maheshwari <87849042+madhav-293@users.noreply.github.com>
2023-02-23 10:03:08 +0530

    Time: 19 ms (99.81%), Space: 95.6 MB (12.33%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,37 +1,38 @@
 class Solution {
-    class Pair implements Comparable<Pair> {
-        int capital, profit;
-
-        public Pair(int capital, int profit) {
-            this.capital = capital;
-            this.profit = profit;
-        }
-
-        public int compareTo(Pair pair) {
-            return capital - pair.capital;
-        }
-    }
-    
-    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        int n = profits.length;
-        Pair [] projects = new Pair[n];
-        for(int i = 0;i<n;i++){
-            projects[i] = new Pair(capital[i],profits[i]);
+    public int findMaximizedCapital(int k, int W, int[] Profits, int[] Capital) {
+        int maxCapital = 0;
+        for(int i = 0 ; i < Capital.length; i++){
+            maxCapital = Math.max(Capital[i], maxCapital);
         }
         
-        Arrays.sort(projects);
-        PriorityQueue<Integer> priority = new PriorityQueue<Integer>( Collections.reverseOrder());
-        int j = 0;
-        int ans = w;
-        for(int i = 0;i<k;i++){
-            while(j<n && projects[j].capital<=ans){
-                priority.add(projects[j++].profit);
+        if(W >= maxCapital){
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(new Comparator<Integer>(){
+                @Override
+                public int compare(Integer a , Integer b){
+                    return a-b;
+                }
+            });
+            for (int p: Profits) {
+                maxHeap.add(p);
+                if (maxHeap.size() > k) maxHeap.poll();    
             }
-            if(priority.isEmpty()){
-                break;
-            }
-            ans+=priority.poll();
+            for (int h: maxHeap) W += h; 
+            return W;
         }
-        return ans;
+        
+        int index;
+        int n = Profits.length;
+        for(int i = 0; i < Math.min(k, n); i++) {
+            index = -1; 
+            for(int j = 0; j < n; ++j) { 
+                if (W >= Capital[j] && (index == -1 || Profits[index] < Profits[j])){
+                        index = j;
+                }
+            }
+            if(index == -1) break;
+            W += Profits[index];
+            Capital[index] = Integer.MAX_VALUE;                
+        }
+        return  W;     
     }
 }
