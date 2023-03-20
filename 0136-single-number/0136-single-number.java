32d992c46705d80fa743a1554218c44faaffacda
Madhav Maheshwari <87849042+madhav-293@users.noreply.github.com>
2023-03-20 20:59:48 +0530

    Time: 6 ms (45.37%), Space: 42.5 MB (51.91%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,13 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
+        if(nums.length==1){
+            return nums[0];
+        }
+        for(int i=0;i<nums.length-1;i+=2){
             if(nums[i]!=nums[i+1]){
                 return nums[i];
             }
-            i++;
         }
-        return nums[nums.length-1];
+       return nums[nums.length-1];
     }
     
 }
