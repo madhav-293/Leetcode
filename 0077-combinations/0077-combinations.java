63581badc1ff31899498819c905750bbb9fce314
Madhav Maheshwari <87849042+madhav-293@users.noreply.github.com>
2023-05-17 20:19:45 +0530

    Time: 3 ms (90.32%), Space: 44.1 MB (49.38%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,18 +1,18 @@
-class Solution {
-    public static List<List<Integer>> combine(int n, int k) {
-		List<List<Integer>> combs = new ArrayList<List<Integer>>();
-		combine(combs, new ArrayList<Integer>(), 1, n, k);
-		return combs;
-	}
-	public static void combine(List<List<Integer>> combs, List<Integer> comb, int start, int n, int k) {
-		if(k==0) {
-			combs.add(new ArrayList<Integer>(comb));
-			return;
-		}
-		for(int i=start;i<=n;i++) {
-			comb.add(i);
-			combine(combs, comb, i+1, n, k-1);
-			comb.remove(comb.size()-1);
-		}
-	}
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (k > n || k < 0) {
+            return result;
+        }
+        if (k == 0) {
+            result.add(new ArrayList<Integer>());
+            return result;
+        }
+        result = combine(n - 1, k - 1);
+        for (List<Integer> list : result) {
+            list.add(n);
+        }
+        result.addAll(combine(n - 1, k));
+        return result;
+    }
 }
