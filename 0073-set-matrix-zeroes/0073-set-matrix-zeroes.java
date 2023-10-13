68c1c25338fb29b18202608da40f570bdcd4188b
Madhav Maheshwari <87849042+madhav-293@users.noreply.github.com>
2023-10-13 12:15:33 +0530

    Time: 1 ms (90.38%), Space: 44.6 MB (50.47%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,37 +1,21 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         int n=matrix.length,m=matrix[0].length;
-        boolean flag=true;
+        int[] row=new int[n];
+        int[] column = new int[m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    fillRow(matrix,i);
-                    fillColumn(matrix,j);
-                    flag=false;
+                    row[i]=1;
+                    column[j]=1;
                 }
             }
         }
-        if(flag) return;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==-1234567){
+                if(row[i]==1 || column[j]==1){
                     matrix[i][j]=0;
                 }
-            }
-        }
-        
-    }
-    public static void fillRow(int[][] matrix,int i){
-        for(int j=0;j<matrix[0].length;j++){
-            if(matrix[i][j]!=0){
-                matrix[i][j]=-1234567;
-            }
-        }
-    }
-    public static void fillColumn(int[][] matrix,int i){
-        for(int j=0;j<matrix.length;j++){
-            if(matrix[j][i]!=0){
-                matrix[j][i]=-1234567;
             }
         }
     }
